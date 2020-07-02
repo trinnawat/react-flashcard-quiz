db8f6bb6c5604e7c2669b5371618fb22c1dbe0cc
Trinnawat Charoenpradubsilp <got.trinnawat@data-alchemist.com>
2020-07-02 16:52:00 +0700

    update changes

---
+++ src/FlashCardList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import FlashCard from './FlashCard'
+
+
+export default function FlashCardList({ flashCards }) {
+    return (
+        <div className="card-grid">
+            {flashCards.map( flashCard => {
+                return <FlashCard flashCard={flashCard} key={flashCard.id} />
+            })}
+        </div>
+    )
+}
